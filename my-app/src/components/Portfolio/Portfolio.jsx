649ed54ca5cc551772feb9f1fd2b583316eb30e9
Egor <goldenyakegor@gmail.com>
2022-03-14 00:59:14 +0300

    add AOS

--- my-app/src/components/Portfolio/Portfolio.jsx
+++ my-app/src/components/Portfolio/Portfolio.jsx
@@ -9,21 +9,21 @@ const Portfolio = () => {
     return (
         <section className="home-portfolio">
             <div className="wrapper-portfolio">
-                <div data-aos="fade-up"  data-aos-duration="5000" className="animate--me animate--init">
+                <div data-aos="fade-up" data-aos-duration="3000" className="animate--me animate--init">
                     <h5>Selected projects</h5>
                     <h1 className="large">Case studies</h1>
                 </div>
                 <div className="list ">
-                    <div data-aos="fade-up" className="list-item-portfolio">
+                    <div data-aos="fade-up" data-aos-duration="3000"  className="list-item-portfolio">
                         <img src={image} alt="image"/>
                     </div>
-                    <div data-aos="fade-up" className="list-item-portfolio">
+                    <div data-aos="fade-up" data-aos-duration="3000" className="list-item-portfolio">
                         <img src={image} alt="image"/>
                     </div>
-                    <div data-aos="fade-up" className="list-item-portfolio">
+                    <div data-aos="fade-up" data-aos-duration="3000" className="list-item-portfolio">
                         <img src={image} alt="image"/>
                     </div>
-                    <div data-aos="fade-up" className="list-item-portfolio">
+                    <div data-aos="fade-up" data-aos-duration="3000" className="list-item-portfolio">
                         <img src={image} alt="image"/>
                     </div>
                 </div>
