eeb377be6a7a7b8b079664006c4b41f53fc55735
Egor <goldenyakegor@gmail.com>
2022-03-14 00:55:15 +0300

    add AOS

--- my-app/src/components/Portfolio/Portfolio.jsx
+++ my-app/src/components/Portfolio/Portfolio.jsx
@@ -1,26 +1,29 @@
 import React from 'react';
 import "./Portfolio.css";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import image from "../../images/portfolio_img.jpeg"
 
 const Portfolio = () => {
+    AOS.init()
     return (
         <section className="home-portfolio">
             <div className="wrapper-portfolio">
-                <div className="animate--me animate--init">
+                <div data-aos="fade-up"  data-aos-duration="5000" className="animate--me animate--init">
                     <h5>Selected projects</h5>
                     <h1 className="large">Case studies</h1>
                 </div>
-                <div className="list">
-                    <div className="list-item-portfolio">
+                <div className="list ">
+                    <div data-aos="fade-up" className="list-item-portfolio">
                         <img src={image} alt="image"/>
                     </div>
-                    <div className="list-item-portfolio">
+                    <div data-aos="fade-up" className="list-item-portfolio">
                         <img src={image} alt="image"/>
                     </div>
-                    <div className="list-item-portfolio">
+                    <div data-aos="fade-up" className="list-item-portfolio">
                         <img src={image} alt="image"/>
                     </div>
-                    <div className="list-item-portfolio">
+                    <div data-aos="fade-up" className="list-item-portfolio">
                         <img src={image} alt="image"/>
                     </div>
                 </div>
