97c13c829a9b902e460dcbcd4f377339d0e6f0c7
Egor <goldenyakegor@gmail.com>
2022-03-15 00:53:38 +0300

    add some functions

--- my-app/src/components/Logo/Logo.jsx
+++ my-app/src/components/Logo/Logo.jsx
@@ -3,7 +3,20 @@ import React from 'react';
 const Logo = () => {
     return (
         <div>
+            <svg width="240" height="60" xmlns="http://www.w3.org/2000/svg">
 
+                <g>
+                    <title>background</title>
+                    <rect fill="none" id="canvas_background" height="62" width="242" y="-1" x="-1"/>
+                </g>
+                <g>
+                    <title>Layer 1</title>
+                    <ellipse ry="22.5" rx="22.5" id="svg_6" cy="28.953125" cx="29" strokeWidth="3" stroke="#000" fill="none"/>
+                    <text fontWeight="bold" xmlSpace="preserve" textAnchor="start" fontFamily="Helvetica, Arial, sans-serif" fontSize="35" id="svg_8" y="41.726557" x="17.5" strokeWidth="0" stroke="#000" fill="#000000">E</text>
+                    <text fontWeight="bold" xmlSpace="preserve" textAnchor="start" fontFamily="Helvetica, Arial, sans-serif" fontSize="24" id="svg_9" y="26.226559" x="67.750022" strokeWidth="0" stroke="#000" fill="#000000">Egor Yakovlev</text>
+                    <text fontStyle="normal" xmlSpace="preserve" textAnchor="start" fontFamily="Helvetica, Arial, sans-serif" fontSize="13" id="svg_10" y="47.726557" x="70.250022" strokeWidth="0" stroke="#000" fill="#000000">FRONTEND-DEVELOPER</text>
+                </g>
+            </svg>
         </div>
     );
 };
