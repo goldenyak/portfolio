93ea21b8f5aad1038c578480dea12745c9bf2545
Egor <goldenyakegor@gmail.com>
2022-03-17 22:33:22 +0300

    add some functions

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -20,7 +20,7 @@ function App() {
             {showMenu && <Menu/>}
             <HomeTop/>
             <PersonalPortfolio/>
-            <Portfolio/>
+            {/*<Portfolio/>*/}
             <HomeDown/>
         </div>
     );
